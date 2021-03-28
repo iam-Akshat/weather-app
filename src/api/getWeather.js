8b8f3d2b5623120b6d848e64f51d70dc162e7ea2
Akshat <masterakshata@gmail.com>
2021-03-28 22:48:35 +0530

    add temperature unit toggling

--- src/api/getWeather.js
+++ src/api/getWeather.js
@@ -1,9 +1,9 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?appid=50724fd12d9b476a0fb49df189e71909&units=metric&q=';
 const loader = document.querySelector('.loader');
 
-const getWeather = async (cityName) => {
+const prepURL = (units, cityName) => `http://api.openweathermap.org/data/2.5/weather?appid=50724fd12d9b476a0fb49df189e71909&units=${units}&q=${cityName}`;
+const getWeather = async (cityName, units) => {
   loader.classList.toggle('d-none');
-  const url = apiURL + cityName;
+  const url = prepURL(units, cityName);
   const res = await fetch(url);
   loader.classList.toggle('d-none');
   if (res.status === 404) return { weather: null, desc: null };
--- src/helper/handleFormSubmit.js
+++ src/helper/handleFormSubmit.js
@@ -5,19 +5,34 @@ import gifFromUrl from './gifFromUrl';
 const form = document.querySelector('form');
 const cityField = document.querySelector('form input');
 const weatherContainer = document.querySelector('.weather-container');
+const units = document.getElementById('units');
+const unitSystem = () => {
+  if (units.checked) {
+    return 'imperial';
+  }
+  return 'metric';
+};
 
+const unitSymbol = () => {
+  if (units.checked) {
+    return 'F';
+  }
+  return 'C';
+};
 const handleFormSubmit = async (e) => {
   e.preventDefault();
   if (form.checkValidity()) {
     weatherContainer.innerHTML = '';
-    const { weather, desc } = await getWeather(cityField.value);
+    const { weather, desc } = await getWeather(cityField.value, unitSystem());
     cityField.value = '';
     if (weather === null) {
       weatherContainer.innerHTML += '<h1>City not found </h1>';
       return;
     }
-    weatherContainer.innerHTML += `<h1>Current temp: ${weather.temp}  °C</h1> `;
+    weatherContainer.innerHTML += `<h1>Current temp: ${weather.temp}  °${unitSymbol()}</h1> `;
+
     const { data: [a] } = await getGiphy(desc.main);
+    weatherContainer.innerHTML += `<h1>Weather is ${desc.main}</h1> `;
     weatherContainer.appendChild(gifFromUrl(a.embed_url));
   } else {
     form.reportValidity();
